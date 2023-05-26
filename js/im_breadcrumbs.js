6e97c2eacc292b0570b792e77f32ce24bd200917
Marius S. Riseth <marius.s.riseth@pm.me>
2023-05-26 18:24:50 +0200

    Testslanking

--- js/im_breadcrumbs.js
+++ js/im_breadcrumbs.js
@@ -1,163 +1,102 @@
 const siteHierarchy = {
    "/index.html": {
       displayName: "Hjem",
-      iconClass: "maskeIndexOutline",
+      iconClass: "maskeOppdragsbankOutline",
       children: {
-         "/timeplan.html": {
-            displayName: "F21 Timeplan",
-            iconClass: "maskeUkerOutline",
+         "/oppdragsbank/1_grafisk-design.html": {
+            displayName: "1. Grafisk design",
+            iconClass: "maskeGrafiskOutline",
             children: {
-               "/timeplan/uke_1.html": { displayName: "Uke 1" },
-               "/timeplan/uke_2.html": { displayName: "Uke 2" },
-               "/timeplan/uke_3.html": { displayName: "Uke 3" },
-               "/timeplan/uke_4.html": { displayName: "Uke 4" },
-               "/timeplan/uke_5.html": { displayName: "Uke 5" },
-               "/timeplan/uke_6.html": { displayName: "Uke 6" },
-               "/timeplan/uke_7.html": { displayName: "Uke 7" },
-               // Vinterferie
-               "/timeplan/uke_9.html": { displayName: "Uke 9" },
-               "/timeplan/uke_10.html": { displayName: "Uke 10" },
-               "/timeplan/uke_11.html": { displayName: "Uke 11" },
-               "/timeplan/uke_12.html": { displayName: "Uke 12" },
-               "/timeplan/uke_13.html": { displayName: "Uke 13" },
-               // Påskseferie
-               "/timeplan/uke_15.html": { displayName: "Uke 15" },
-               "/timeplan/uke_16.html": { displayName: "Uke 16" },
-               "/timeplan/uke_17.html": { displayName: "Uke 17" },
-               "/timeplan/uke_18.html": { displayName: "Uke 18" },
-               "/timeplan/uke_19.html": { displayName: "Uke 19" },
-               "/timeplan/uke_20.html": { displayName: "Uke 20" },
-               "/timeplan/uke_21.html": { displayName: "Uke 21" },
-               "/timeplan/uke_22.html": { displayName: "Uke 22" },
-               "/timeplan/uke_23.html": { displayName: "Uke 23" },
-               "/timeplan/uke_24.html": { displayName: "Uke 24" },
-               "/timeplan/uke_25.html": { displayName: "Uke 25" },
-               // Sommerferie
-               "/timeplan/uke_34.html": { displayName: "Uke 34" },
-               "/timeplan/uke_35.html": { displayName: "Uke 35" },
-               "/timeplan/uke_36.html": { displayName: "Uke 36" },
-               "/timeplan/uke_37.html": { displayName: "Uke 37" },
-               "/timeplan/uke_38.html": { displayName: "Uke 38" },
-               "/timeplan/uke_39.html": { displayName: "Uke 39" },
-               "/timeplan/uke_40.html": { displayName: "Uke 40" },
-               "/timeplan/uke_41.html": { displayName: "Uke 41" },
-               "/timeplan/uke_42.html": { displayName: "Uke 42" },
-               "/timeplan/uke_43.html": { displayName: "Uke 43" },
-               "/timeplan/uke_44.html": { displayName: "Uke 44" },
-               "/timeplan/uke_45.html": { displayName: "Uke 45" },
-               "/timeplan/uke_46.html": { displayName: "Uke 46" },
-               "/timeplan/uke_47.html": { displayName: "Uke 47" },
-               "/timeplan/uke_48.html": { displayName: "Uke 48" },
-               "/timeplan/uke_49.html": { displayName: "Uke 49" },
-               "/timeplan/uke_50.html": { displayName: "Uke 50" },
-               "/timeplan/uke_51.html": { displayName: "Uke 51" },
-               // Juleferie
+               "/oppdragsbank/1_grafisk-design/1-1-1_illustrator-pen-tool.html": { displayName: "Illustrator: Pen tool" },
+               "/oppdragsbank/1_grafisk-design/1-2-1_produkt-etter-manualen.html": { displayName: "Produkt etter manualen" },
+               "/oppdragsbank/1_grafisk-design/1-2-2_serie-etter-manualen.html": { displayName: "Serie etter manualen" },
+               "/oppdragsbank/1_grafisk-design/1-2-3_fiktiv-virksomhet.html": { displayName: "Fiktiv virksomhet" },
+               "/oppdragsbank/1_grafisk-design/1-1-2_deg-selv-i-vektor.html": { displayName: "Deg selv i vektor" },
+               "/oppdragsbank/1_grafisk-design/1-2-4_im-i-vektor.html": { displayName: "IM-lærerne i vektor" },
             },
          },
-         "/oppdragsbank.html": {
-            displayName: "Oppdragsbank",
-            iconClass: "maskeOppdragsbankOutline",
+         "/oppdragsbank/2_3d-design.html": {
+            displayName: "2. 3D-design",
+            iconClass: "maskeTreDOutline",
             children: {
-               "/oppdragsbank/1_grafisk-design.html": {
-                  displayName: "1. Grafisk design",
-                  iconClass: "maskeGrafiskOutline",
-                  children: {
-                     "/oppdragsbank/1_grafisk-design/1-1-1_illustrator-pen-tool.html": { displayName: "Illustrator: Pen tool" },
-                     "/oppdragsbank/1_grafisk-design/1-2-1_produkt-etter-manualen.html": { displayName: "Produkt etter manualen" },
-                     "/oppdragsbank/1_grafisk-design/1-2-2_serie-etter-manualen.html": { displayName: "Serie etter manualen" },
-                     "/oppdragsbank/1_grafisk-design/1-2-3_fiktiv-virksomhet.html": { displayName: "Fiktiv virksomhet" },
-                     "/oppdragsbank/1_grafisk-design/1-1-2_deg-selv-i-vektor.html": { displayName: "Deg selv i vektor" },
-                     "/oppdragsbank/1_grafisk-design/1-2-4_im-i-vektor.html": { displayName: "IM-lærerne i vektor" },
-                  },
-               },
-               "/oppdragsbank/2_3d-design.html": {
-                  displayName: "2. 3D-design",
-                  iconClass: "maskeTreDOutline",
-                  children: {
-                     "/oppdragsbank/2_3d-design/2-1-1_blenderkurs.html": { displayName: "Lær å bruke Blender" },
-                     "/oppdragsbank/2_3d-design/2-2-1_modeller-spillobjekt.html": { displayName: "Modeller et spillobjekt" },
-                     "/oppdragsbank/2_3d-design/2-2-2_produktdesign.html": { displayName: "Produktdesign: PC-kabinett" },
-                  },
-               },
-               "/oppdragsbank/3_fotografi.html": {
-                  displayName: "3. Fotografi",
-                  iconClass: "maskeFotoOutline",
-                  children: {
-                     "/oppdragsbank/3_fotografi/3-1-1_naturens-barn.html": { displayName: "Naturens barn" },
-                     "/oppdragsbank/3_fotografi/3-2-1_filmplakat.html": { displayName: "Filmplakat" },
-                     "/oppdragsbank/3_fotografi/3-2-2_gatefoto.html": { displayName: "Gatefoto" },
-                  },
-               },
-               "/oppdragsbank/4_film-og-vfx.html": {
-                  displayName: "4. Film og VFX",
-                  iconClass: "maskeFilmOutline",
-                  children: {
-                     "/oppdragsbank/4_film-og-vfx/4-2-1_supersmooth-reklamefilm.html": { displayName: "Reklamefilm" },
-                     "/oppdragsbank/4_film-og-vfx/4-2-2_manus123.html": { displayName: "Manus 1-2-3" },
-                     "/oppdragsbank/4_film-og-vfx/4-2-3_konkurranserprodukt.html": { displayName: "Filmkonkurranse" },
-                     "/oppdragsbank/4_film-og-vfx/4-1-1_verdensrommet-animert.html": { displayName: "Verdensrommet Animert" },
-                  },
-               },
-               "/oppdragsbank/5_animasjon.html": {
-                  displayName: "5. Animasjon",
-                  iconClass: "maskeAnimasjon",
-                  children: {
-                     "/oppdragsbank/5_animasjon/5-2-1_tine-animasjon.html": { displayName: "Litagoreklame" },
-                     "/oppdragsbank/5_animasjon/5-1-1_walkcycle.html": { displayName: "Walk cycle" },
-                  },
-               },
-               "/oppdragsbank/6_lyddesign.html": {
-                  displayName: "6. Lyddesign",
-                  iconClass: "maskeLydOutline",
-                  children: {
-                     "/oppdragsbank/6_lyddesign/6-2-1_im-podcast.html": { displayName: "IM-Podkast" },
-                     "/oppdragsbank/6_lyddesign/6-2-2_musicless-music-video.html": { displayName: "Musicless music video" },
-                     "/oppdragsbank/6_lyddesign/6-1-1_analyse-av-lyd-i-film.html": { displayName: "Analyse av lyd i film" },
-                  },
-               },
-               "/oppdragsbank/7_drift.html": {
-                  displayName: "7. Drift",
-                  iconClass: "maskeDriftOutline",
-                  children: {
-                     "/oppdragsbank/7_drift/7-2-1_burp-suite.html": { displayName: "Burp Suite" },
-                     "/oppdragsbank/7_drift/7-2-2_bash-script.html": { displayName: "Bash Script" },
-                     // ...
-                  },
-               },
-               "/oppdragsbank/8_programmering.html": {
-                  displayName: "8. Programmering",
-                  iconClass: "maskeUtviklingOutline",
-                  children: {
-                     "/oppdragsbank/8_programmering/8-2-1_pihole-blokade.html": { displayName: "PiHole Blokade" },
-                  },
-               },
-               "/oppdragsbank/9_webutvikling.html": {
-                  displayName: "9. Webutvikling",
-                  iconClass: "maskeWebOutline",
-                  children: {
-                     "/oppdragsbank/9_webutvikling/9-2-1_reklamefilmnettside.html": { displayName: "Reklamefilmnettside" },
-                     "/oppdragsbank/9_webutvikling/9-2-2_kantinenettside.html": { displayName: "Kantinenettside" },
-                     "/oppdragsbank/9_webutvikling/9-2-3_kantinemeny.html": { displayName: "Kantinemeny" },
-                     "/oppdragsbank/9_webutvikling/9-2-4_abstrakt-kunst.html": { displayName: "Abstrakt kunst" },
-                     "/oppdragsbank/9_webutvikling/9-2-6_datamattebase.html": { displayName: "Datamattebase" },
-                  },
-               },
-               "/oppdragsbank/10_ai-systemer.html": {
-                  displayName: "10. AI-systemer",
-                  iconClass: "maskeAiOutline",
-                  children: {
-                     "/oppdragsbank/10_ai-systemer/10-1-1_stable-diffusion.html": { displayName: "Lær å Bruke SD" },
-                  },
-               },
-               "/oppdragsbank/visuell-logg.html": {
-                  displayName: "Visuell logg",
-                  iconClass: "maskeLoggOutline",
-               },
+               "/oppdragsbank/2_3d-design/2-1-1_blenderkurs.html": { displayName: "Lær å bruke Blender" },
+               "/oppdragsbank/2_3d-design/2-2-1_modeller-spillobjekt.html": { displayName: "Modeller et spillobjekt" },
+               "/oppdragsbank/2_3d-design/2-2-2_produktdesign.html": { displayName: "Produktdesign: PC-kabinett" },
             },
          },
-         "/ressursbank.html": {
-            displayName: "IM-lærerverket",
-            iconClass: "maskeFagverket",
+         "/oppdragsbank/3_fotografi.html": {
+            displayName: "3. Fotografi",
+            iconClass: "maskeFotoOutline",
+            children: {
+               "/oppdragsbank/3_fotografi/3-1-1_naturens-barn.html": { displayName: "Naturens barn" },
+               "/oppdragsbank/3_fotografi/3-2-1_filmplakat.html": { displayName: "Filmplakat" },
+               "/oppdragsbank/3_fotografi/3-2-2_gatefoto.html": { displayName: "Gatefoto" },
+            },
+         },
+         "/oppdragsbank/4_film-og-vfx.html": {
+            displayName: "4. Film og VFX",
+            iconClass: "maskeFilmOutline",
+            children: {
+               "/oppdragsbank/4_film-og-vfx/4-2-1_supersmooth-reklamefilm.html": { displayName: "Reklamefilm" },
+               "/oppdragsbank/4_film-og-vfx/4-2-2_manus123.html": { displayName: "Manus 1-2-3" },
+               "/oppdragsbank/4_film-og-vfx/4-2-3_konkurranserprodukt.html": { displayName: "Filmkonkurranse" },
+               "/oppdragsbank/4_film-og-vfx/4-1-1_verdensrommet-animert.html": { displayName: "Verdensrommet Animert" },
+            },
+         },
+         "/oppdragsbank/5_animasjon.html": {
+            displayName: "5. Animasjon",
+            iconClass: "maskeAnimasjon",
+            children: {
+               "/oppdragsbank/5_animasjon/5-2-1_tine-animasjon.html": { displayName: "Litagoreklame" },
+               "/oppdragsbank/5_animasjon/5-1-1_walkcycle.html": { displayName: "Walk cycle" },
+            },
+         },
+         "/oppdragsbank/6_lyddesign.html": {
+            displayName: "6. Lyddesign",
+            iconClass: "maskeLydOutline",
+            children: {
+               "/oppdragsbank/6_lyddesign/6-2-1_im-podcast.html": { displayName: "IM-Podkast" },
+               "/oppdragsbank/6_lyddesign/6-2-2_musicless-music-video.html": { displayName: "Musicless music video" },
+               "/oppdragsbank/6_lyddesign/6-1-1_analyse-av-lyd-i-film.html": { displayName: "Analyse av lyd i film" },
+            },
+         },
+         "/oppdragsbank/7_drift.html": {
+            displayName: "7. Drift",
+            iconClass: "maskeDriftOutline",
+            children: {
+               "/oppdragsbank/7_drift/7-2-1_burp-suite.html": { displayName: "Burp Suite" },
+               "/oppdragsbank/7_drift/7-2-2_bash-script.html": { displayName: "Bash Script" },
+               // ...
+            },
+         },
+         "/oppdragsbank/8_programmering.html": {
+            displayName: "8. Programmering",
+            iconClass: "maskeUtviklingOutline",
+            children: {
+               "/oppdragsbank/8_programmering/8-2-1_pihole-blokade.html": { displayName: "PiHole Blokade" },
+            },
+         },
+         "/oppdragsbank/9_webutvikling.html": {
+            displayName: "9. Webutvikling",
+            iconClass: "maskeWebOutline",
+            children: {
+               "/oppdragsbank/9_webutvikling/9-2-1_reklamefilmnettside.html": { displayName: "Reklamefilmnettside" },
+               "/oppdragsbank/9_webutvikling/9-2-2_kantinenettside.html": { displayName: "Kantinenettside" },
+               "/oppdragsbank/9_webutvikling/9-2-3_kantinemeny.html": { displayName: "Kantinemeny" },
+               "/oppdragsbank/9_webutvikling/9-2-4_abstrakt-kunst.html": { displayName: "Abstrakt kunst" },
+               "/oppdragsbank/9_webutvikling/9-2-6_datamattebase.html": { displayName: "Datamattebase" },
+            },
+         },
+         "/oppdragsbank/10_ai-systemer.html": {
+            displayName: "10. AI-systemer",
+            iconClass: "maskeAiOutline",
+            children: {
+               "/oppdragsbank/10_ai-systemer/10-1-1_stable-diffusion.html": { displayName: "Lær å Bruke SD" },
+            },
+         },
+         "/oppdragsbank/visuell-logg.html": {
+            displayName: "Visuell logg",
+            iconClass: "maskeLoggOutline",
          },
       },
    },
